5df10e05ce24a62432118437b60a1e2b19e9b071
shicr <shicr@byu.edu>
2025-04-09 11:31:56 -0600

    cahnges

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer () {
     return (
         <footer className="bg-gray-800 text-white py-4 text-center">
             <p className="text-sm">© 2025 CineNiche. All rights reserved.</p>
-            <center><p><a href ="/policy">Privacy Policy</a></p></center>
+            <center><p><a href ="/policy" style={{ color: '#e5c78c' }}  >Privacy Policy</a></p></center>
         </footer>
   
     )
