00af4ed66de1790e1c935eab2fd52679f2f85853
shicr <shicr@byu.edu>
2025-04-09 14:35:58 -0600

    :)

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './RegisterPage.css';
+
 
 function Register() {
   const [email, setEmail] = useState('');
