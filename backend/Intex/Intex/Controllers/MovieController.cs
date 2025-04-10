1b313a16dd74b6d664d42a7a6a618a8a62896877
Channing Paxman <channing.paxman@gmail.com>
2025-04-10 10:20:18 -0600

    home page top 10

--- backend/Intex/Intex/Controllers/MovieController.cs
+++ backend/Intex/Intex/Controllers/MovieController.cs
@@ -295,6 +295,19 @@ namespace Intex.Controllers
         //         totalPages
         //     });
         // }
+        
+        [HttpGet("GetMoviesByTitles")]
+        public async Task<IActionResult> GetMoviesByTitles([FromQuery] List<string> titles)
+        {
+            if (titles == null || !titles.Any())
+                return BadRequest("No titles provided.");
+
+            var matchedMovies = await _savedMovieContext.movies_titles
+                .Where(m => titles.Contains(m.title))
+                .ToListAsync();
+
+            return Ok(matchedMovies);
+        }
 
 
         [HttpGet("GetMovieTypes")]
